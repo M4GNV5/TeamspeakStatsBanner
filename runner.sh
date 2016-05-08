0dc5ed22c4854738b3cda88239c6850ed77f8084
M4GNV5 <eragonjml@googlemail.com>
2016-05-08 21:29:16 +0200

    initial commit

---
+++ runner.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+while true
+do
+	for i in {1..30}
+	do
+		free -m | awk 'NR==2{printf "%f", $3*100/$2}' >> data
+		echo -n " " >> data
+		top -bn1 | grep load | awk '{printf "%f", $(NF-2)}' >> data
+		echo "" >> data
+		echo $i
+		sleep 1
+	done
+
+	echo -n "" > news
+	curl -H "X-Authorization: YOUR-KEY-HERE" http://api.zeit.de/content\?limit\=3 > rawnews
+
+	cat rawnews | jq '.matches | .[0].title' >> news
+	cat rawnews | jq '.matches | .[0].subtitle' >> news
+	cat rawnews | jq '.matches | .[1].title' >> news
+	cat rawnews | jq '.matches | .[1].subtitle' >> news
+	cat rawnews | jq '.matches | .[2].title' >> news
+	cat rawnews | jq '.matches | .[2].subtitle' >> news
+
+	uptime > uptime
+
+	../PointerScript/bin/ptrs render.ptrs
+	exit 1
+
+	echo "" > data
+done
