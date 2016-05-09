57ca79827e012ead9ff943d090eb80f3a421737c
M4GNV5 <eragonjml@googlemail.com>
2016-05-09 20:43:40 +0100

    plot memory with and without cache

--- collectData.sh
+++ collectData.sh
@@ -2,9 +2,11 @@
 
 echo -n "" > data/cpu.dat
 echo -n "" > data/mem.dat
+echo -n "" > data/cache.dat
 
 while true
 do
-	free -m | awk 'NR==2{printf "%f\n", $3*100/$2}' >> data/mem.dat
+	free | awk 'NR==2{print $3 * 100 / $2}' >> data/cache.dat
+	free | awk 'NR == 2 { a = $2 } NR == 3 { print (a - $4) * 100 / a }' >> data/mem.dat
 	top -bn2 | grep %Cpu | tail -1 | awk '{printf "%f\n", $2}' >> data/cpu.dat
 done
