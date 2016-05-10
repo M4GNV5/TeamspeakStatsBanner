5d77844bb340b7314b9b13bdd00ba31f0b3ab710
M4GNV5 <eragonjml@googlemail.com>
2016-05-10 20:44:19 +0100

    small imrpovements on how to display data

--- runner.sh
+++ runner.sh
@@ -9,14 +9,16 @@ do
 	echo -n "" > data/news.dat
 	curl -H "X-Authorization: YOUR-KEY-HERE" http://api.zeit.de/content\?limit\=3 > data/rawnews.dat
 
-	cat data/rawnews.dat | jq '.matches | .[0].title' >> data/news.dat
-	cat data/rawnews.dat | jq '.matches | .[0].subtitle' >> data/news.dat
-	cat data/rawnews.dat | jq '.matches | .[1].title' >> data/news.dat
-	cat data/rawnews.dat | jq '.matches | .[1].subtitle' >> data/news.dat
-	cat data/rawnews.dat | jq '.matches | .[2].title' >> data/news.dat
-	cat data/rawnews.dat | jq '.matches | .[2].subtitle' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[0].title' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[0].subtitle' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[1].title' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[1].subtitle' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[2].title' >> data/news.dat
+	cat data/rawnews.dat | jq -r '.matches | .[2].subtitle' >> data/news.dat
 
-	uptime > data/uptime.dat
+	echo -n "time" > data/uptime.dat
+	uptime >> data/uptime.dat
 
 	../PointerScript/bin/ptrs render.ptrs
+	cp banner.png ~/screenshot/files/banner.png
 done
